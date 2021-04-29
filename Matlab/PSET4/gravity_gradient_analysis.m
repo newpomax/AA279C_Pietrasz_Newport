feed327b1259071f2491b14efb6f73e0aeacb7ca
Valerie <valerie.pietrasz@gmail.com>
2021-04-29 08:06:01 -0700

    Added cleaned-up gravity gradient analysis. Still TODO alternative stable configs.

--- Matlab/PSET4/gravity_gradient_analysis.m
+++ Matlab/PSET4/gravity_gradient_analysis.m
@@ -56,6 +56,54 @@ M_est_nonaligned = (3*sim_constants.mu_Earth/norm(R)^5)* ...
     [(I(3) - I(2))*R(2)*R(3);
      (I(1) - I(3))*R(3)*R(1);
      (I(2) - I(1))*R(1)*R(2)]
+ 
+ %% Stability
+ 
+kR = (I(3)-I(2))/I(1)
+kT = (I(3)-I(1))/I(2)
+kN = (I(2)-I(1))/I(3)
+
+figure();
+hold on;
+
+% unstable pitch
+patch([-1, -1, 1], [-1, 1, 1], 'y', 'FaceAlpha', .4);
+text(0.1, 0.7, 'Unstable pitch'); 
+
+% unstable yaw
+patch([0, 0, 1, 1], [-1, 0, 0, -1], 'b', 'FaceAlpha', .4);
+patch([0, 0, -1, -1], [1, 0, 0, 1], 'b', 'FaceAlpha', .4);
+text(0.4, -0.5, 'Unstable yaw'); 
+
+% unstable roll
+fp = fimplicit(@(x,y) 1 + 3*x + x.*y - 4*sqrt(x.*y), [-1, 0, -1, 0], 'k'); 
+fp_x = fp.XData;
+fp_y = fp.YData;
+patch([fp_x fliplr(fp_x)], [fp_y -1*ones(1,length(fp_y))], ...
+    'r', 'FaceAlpha', .4, 'EdgeAlpha', 0)
+% Estimate the rest of the curve with a parabola
+unstable_roll_x = [-1:0.01:-(1/3) -(1/3)];
+unstable_roll_y = -0.2*(unstable_roll_x+1/3).^2;
+plot(unstable_roll_x, unstable_roll_y, 'k');
+patch([unstable_roll_x, fliplr(unstable_roll_x)], ...
+    [unstable_roll_y -1*ones(1,length(unstable_roll_y))], ...
+    'r', 'FaceAlpha', .4, 'EdgeAlpha', 0);
+text(-0.5, -0.75, 'Unstable roll'); 
+
+% Satellite
+plot([kT], [kR], 'k.', 'MarkerSize', 25);
+text_string = strcat('LS2 (-', num2str(round(kT, 5)), ', ', ...
+    num2str(round(kR, 5)), ')');
+text(0.6, 0.93, text_string); 
+
+% Alternative options
+% Still need to finish
+
+title('Gravity gradient stability diagram');
+xlabel('k_T');
+ylabel('k_R');
+xlim([-1,1]);
+ylim([-1,1]);
 
 %% Run + process sim
 sim('Propagator');
