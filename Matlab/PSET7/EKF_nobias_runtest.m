28140d5e940fa213a719557012a4b4c1fc1a01cd
Max Newport <newpomax@gmail.com>
2021-05-16 22:40:24 -0700

    Added no-bias version of EKF, which seems to work pretty well.

---
+++ Matlab/PSET7/EKF_nobias_runtest.m
@@ -0,0 +1,92 @@
+%% EKF Testing Script
+close all
+clear all
+
+% Add path to subfolders
+addpath(fullfile('..', 'functions_and_helper_scripts'));
+
+% Load simulation data
+load('EKF_testing.mat');
+w_meas = squeeze(w_meas.Data).';
+B_meas = B_meas.Data;
+B_meas = B_meas;%./(sqrt(sum(B_meas.^2,2))*ones(1,3)); % convert to just direction
+Sazel_meas = Sazel_meas.Data;
+B_ECI = B_ECI.Data;
+B_ECI = B_ECI;%./(sqrt(sum(B_ECI.^2,2))*ones(1,3)); % convert to just direction
+S_ECI = S_ECI.Data;
+M = M_perturbations.Data; % torque input (should be p small generally)
+
+% Add new sim_constants items
+sim_constants.sunsensor_rotm = eye(3); % sun sensor frame = princ axes
+sim_constants.mu0 = [0;0;0;1;0.01;0.01;0.01]; % initial estimate
+sim_constants.cov0 = eye(7); % initial covariance
+sim_constants.Q = 0.1*sim_constants.time_step*eye(7); % process noise
+sim_constants.R = eye(8); % measurement noise 
+sim_constants.R(1:3,1:3) = diag(sim_constants.gyro_error);
+sim_constants.R(4:6,4:6) = diag(sim_constants.mag_error);
+sim_constants.R(7:8,7:8) = diag(sim_constants.ss_error);
+
+% Run EKF simulator
+sim('EKF_noBias_Testing');
+
+%% Get estimated attitude in Euler312 angles
+A_EKF = zeros(length(q_EKF.Data),3,3);
+e_EKF = zeros(length(q_EKF.Data),3);
+for i = 1:length(e_EKF)
+    myq = q_EKF.Data(i,:);
+    myq = myq/norm(myq);
+    myA = q2A(myq.');
+   A_EKF(i,:,:) = myA;
+   e_EKF(i,:) = A2e(myA);
+end
+
+%% Plot stuff
+time_label = ['Time [', num2str(plot_format.time_increments), ']'];
+df = plot_format.downsample_factor;
+time_df = downsample(sim_output.time,df);
+figure('Name','Attitude Estimate'); hold on;
+labs = {'\phi', '\theta', '\psi'};
+
+for i = 1:3
+   subplot(1,3,i); hold on;
+   plot(time_df,wrapTo180(rad2deg(downsample(e_estq.Data(:,i),df))), ':', 'Linewidth',1,'DisplayName','q method');
+   plot(time_df,wrapTo180(rad2deg(downsample(e.Data(:,i),df))),'LineWidth',1,'DisplayName','Attitude Angle');
+   plot(time_df,wrapTo180(rad2deg(downsample(e_EKF(:,i),df))),':','LineWidth',1.5,'DisplayName','EKF');
+   legend; ylabel([labs{i} ', deg']); xlabel(time_label);
+   title_text = [labs{i} ' of ', plot_format.mission_name];
+   title(title_text); legend('location','best');
+end
+
+figure('Name','Ang Vel Estimate'); hold on;
+labs = {'\omega_x', '\omega_y', '\omega_z'};
+for i = 1:3
+   subplot(1,3,i); hold on;
+   plot(time_df,wrapTo180(rad2deg(downsample(w_meas(:,i),df))), ':', 'Linewidth',1,'DisplayName','Measured \omega');
+   plot(time_df,wrapTo180(rad2deg(downsample(w.Data(:,i),df))),'LineWidth',1,'DisplayName','True \omega');
+   plot(time_df,wrapTo180(rad2deg(downsample(w_EKF.Data(:,i),df))),':','LineWidth',1.5,'DisplayName','EKF');
+   legend; ylabel([labs{i} ', rad/s']); xlabel(time_label);
+   title_text = [labs{i} ' of ', plot_format.mission_name];
+   title(title_text); legend('location','best');
+end
+
+figure('Name','Quat Resids'); hold on;
+labs = {'q_1', 'q_2', 'q_3', 'q_4'};
+for i = 1:4
+   subplot(1,4,i); hold on;
+   plot(time_df,downsample(z_pre.Data(:,i),df), ':', 'Linewidth',1,'DisplayName','Pre-fit Difference');
+   plot(time_df,downsample(z_post.Data(:,i),df), ':', 'Linewidth',1,'DisplayName','Post-fit Difference');
+   legend; ylabel([labs{i}]); xlabel(time_label);
+   title_text = [labs{i} ' residuals of ', plot_format.mission_name];
+   title(title_text); legend('location','best');
+end
+
+figure('Name','Ang Vel Resids'); hold on;
+labs = {'\omega_1', '\omega_2', '\omgea_3'};
+for i = 1:3
+   subplot(1,4,i); hold on;
+   plot(time_df,downsample(z_pre.Data(:,i+4),df), ':', 'Linewidth',1,'DisplayName','Pre-fit Difference');
+   plot(time_df,downsample(z_post.Data(:,i+4),df), ':', 'Linewidth',1,'DisplayName','Post-fit Difference');
+   legend; ylabel([labs{i}]); xlabel(time_label);
+   title_text = [labs{i} ' residuals of ', plot_format.mission_name];
+   title(title_text); legend('location','best');
+end
--- Matlab/PSET7/EKF_runtest.m
+++ Matlab/PSET7/EKF_runtest.m
@@ -6,13 +6,13 @@ clear all
 addpath(fullfile('..', 'functions_and_helper_scripts'));
 
 % Load simulation data
-load('EKF_testing_nonoise.mat');
+load('EKF_testing.mat');
 w_meas = squeeze(w_meas.Data).';
 B_meas = B_meas.Data;
-B_meas = B_meas./(sqrt(sum(B_meas.^2,2))*ones(1,3)); % convert to just direction
+B_meas = B_meas;%./(sqrt(sum(B_meas.^2,2))*ones(1,3)); % convert to just direction
 Sazel_meas = Sazel_meas.Data;
 B_ECI = B_ECI.Data;
-B_ECI = B_ECI./(sqrt(sum(B_ECI.^2,2))*ones(1,3)); % convert to just direction
+B_ECI = B_ECI;%./(sqrt(sum(B_ECI.^2,2))*ones(1,3)); % convert to just direction
 S_ECI = S_ECI.Data;
 M = M_perturbations.Data; % torque input (should be p small generally)
 
@@ -21,11 +21,10 @@ sim_constants.sunsensor_rotm = eye(3); % sun sensor frame = princ axes
 sim_constants.mu0 = [0;0;0;1;0.01;0.01;0.01;0;0;0;0;0;0;0;0]; % initial estimate
 sim_constants.cov0 = eye(15); % initial covariance
 sim_constants.Q = 0.1*sim_constants.time_step*eye(15); % process noise
-sim_constants.R = eye(8); % measurement noise (will need updated for new sensor measurements)
+sim_constants.R = eye(8); % measurement noise 
 sim_constants.R(1:3,1:3) = diag(sim_constants.gyro_error);
 sim_constants.R(4:6,4:6) = diag(sim_constants.mag_error);
 sim_constants.R(7:8,7:8) = diag(sim_constants.ss_error);
-% sim_constants.R = zeros(8);
 
 % Run EKF simulator
 sim('EKF_Testing');
@@ -34,7 +33,9 @@ sim('EKF_Testing');
 A_EKF = zeros(length(q_EKF.Data),3,3);
 e_EKF = zeros(length(q_EKF.Data),3);
 for i = 1:length(e_EKF)
-    myA = q2A(q_EKF.Data(i,:).');
+    myq = q_EKF.Data(i,:);
+    myq = myq/norm(myq);
+    myA = q2A(myq.');
    A_EKF(i,:,:) = myA;
    e_EKF(i,:) = A2e(myA);
 end
@@ -69,3 +70,32 @@ for i = 1:3
    title(title_text); legend('location','best');
 end
 
+figure('Name','\omega Bias Estimate'); hold on;
+labs = {'b_{\omega,x}', 'b_{\omega,y}', 'b_{\omega,z}'};
+for i = 1:3
+   subplot(1,3,i); hold on;
+   plot(time_df,downsample(bw.Data(:,i),df), ':', 'Linewidth',1,'DisplayName','Measured \omega bias');
+   legend; ylabel([labs{i} ', rad/s']); xlabel(time_label);
+   title_text = [labs{i} ' of ', plot_format.mission_name];
+   title(title_text); 
+end
+
+figure('Name','Magnetometer Bias Estimate'); hold on;
+labs = {'b_{B,x}', 'b_{B,y}', 'b_{B,z}'};
+for i = 1:3
+   subplot(1,3,i); hold on;
+   plot(time_df,downsample(bB.Data(:,i),df), ':', 'Linewidth',1,'DisplayName','Measured bias');
+   legend; ylabel([labs{i} ', Tesla']); xlabel(time_label);
+   title_text = [labs{i} ' of ', plot_format.mission_name];
+   title(title_text); 
+end
+
+figure('Name','Sun Sensor Bias Estimate'); hold on;
+labs = {'b_{az}', 'b_{el}'};
+for i = 1:2
+   subplot(1,2,i); hold on;
+   plot(time_df,downsample(rad2deg(bS.Data(:,i)),df), ':', 'Linewidth',1,'DisplayName','Measured bias');
+   legend; ylabel([labs{i} ', deg']); xlabel(time_label);
+   title_text = [labs{i} ' of ', plot_format.mission_name];
+   title(title_text); 
+end
